c256eb2b79c2d7dbea1afae5ffe953b6119f749f
david rogers <rogersdpdr@gmail.com>
2022-11-29 18:28:45 +0100

    add overlay on edit

--- src/components/track_entry.jsx
+++ src/components/track_entry.jsx
@@ -1,9 +1,13 @@
+import { InputField, InputButton } from "./input_field";
 import { Show, createSignal } from "solid-js";
+import { Portal } from "solid-js/web";
 
 export default function TrackEntry(props) {
+  let title, genre, year, artists, totalSeconds;
   const song = props.song;
   const toggleVideo = () => setShowVideo(!showVideo())
   const [showVideo, setShowVideo] = createSignal(false);
+  const [edit, setEdit] = createSignal(false);
   var minutes = Math.floor(song.lengthSeconds / 60);
   var seconds = song.lengthSeconds - (minutes * 60);
   var correctedSeconds = 
@@ -15,8 +19,59 @@ export default function TrackEntry(props) {
 
   function handleDownload() {}
 
+  function toggleEdit(event) { setEdit(!edit()) }
+
+  function removeClick(event) { event.stopPropagation() }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(title.value);
+    console.log(genre.value);
+    console.log(year.value);
+    console.log(artists.value);
+    
+    console.warn("updated");
+  }
+
+  function handleAutoFill() { console.log("autofilling") }
+
   return (
     <>
+    <Show when={edit()} fallback={<div/>}>
+    <Portal>
+      <div class="overlay" onClick={toggleEdit}>
+        <div class="card popup-content" onClick={removeClick}>
+          <form onSubmit={handleSubmit} class="m-2">
+            <InputField ref={title} label="Title" type="text" 
+            value={song.title} placeholder="Title"/>
+            <InputField ref={artists} label="Artist" type="text" 
+            value={song.artist} placeholder="Artist"/>
+            <p class="help">a list seperated by semicolons(; )</p>
+            <InputField ref={genre} label="Genre" type="text" 
+            value={song.genre} placeholder="Genre"/>
+            <div class="field is-horizontal">
+              <div class="label px-3">Year</div>
+              <div class="control">
+                <input class="input" type="number" ref={year} placeholder="Year"/>
+              </div>
+              <div class="label px-3">Seconds</div>
+              <div class="control">
+                <input class="input" type="number" ref={totalSeconds} placeholder="Seconds"/>
+              </div>
+            </div>
+            <div class="field is-grouped">
+              <div class="control">
+                <input class="button" type="submit" value="Update"/>
+              </div>
+              <div class="control">
+                <div class="button" onClick={handleAutoFill}> Auto Fill </div> 
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Portal>
+  </Show>
     <div class="card m-5">
       <div class="columns">
         <div class="column is-3">
@@ -45,7 +100,7 @@ export default function TrackEntry(props) {
           </Show>
           <div class="columns">
             <div class="column">
-              <button class="button mx-3">
+              <button class="button mx-3" onClick={toggleEdit}>
                 Edit
               </button>
               <button class="button mx-3">
@@ -62,6 +117,6 @@ export default function TrackEntry(props) {
         </div>
       </div>
     </div>
-  </>
+</>
   )
 }
